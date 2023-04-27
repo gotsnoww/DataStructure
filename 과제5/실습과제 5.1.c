e7c1a4a94a132adcf7c416bed72da263bfd5ef32
gotsnoww <127213266+gotsnoww@users.noreply.github.com>
2023-04-27 12:02:02 +0900

    Update 실습과제 5.1.c
    배열 스택 자료구조를 통해 구현된 배열 스택 프로그램이다.
    자료구조 내에는 스택을 할당할 공간(배열)과 스택의 삽입과 삭제가 이루어질 최상단 데이터인 top index가 포함되어 있다.
    MyArrayStack.h 헤더파일은 많은 함수로 구현되어 있다.
    스택 초기화, data push, pop, 스택의 최상단 데이터 확인, 스택이 비었는지, 가득 차있는지를 확인하는 함수가 존재한다.

--- 과제5/실습과제 5.1.c
+++ 과제5/실습과제 5.1.c
@@ -1,3 +1,71 @@
+//MyArrayStack.h
+#pragma once
+#ifndef __MY_ARRAY_STACK_H__
+#define __MY_ARRAY_STACK_H__
+#include <stdio.h>
+#include <stdlib.h>
+#define STACK_LEN 100
+typedef enum { F, T }Bool; //Bool 타입 정의
+typedef int Data; //Data 타입 정의
+typedef struct MyArrayStack { //배열 스택의 자료구조
+	Data arr[STACK_LEN];
+	int top;
+}ArrStack; //Array Stack 구조체 선언
+void Stack_Init(ArrStack* pStack); //배열 스택 초기화
+void Stack_Push(ArrStack* pStack, Data item); //데이터 삽입
+Data Stack_Pop(ArrStack* pStack); //데이터 인출
+Data Stack_Peek(ArrStack* pStack); //최상단 데이터 확인
+Bool Stack_IsEmpty(ArrStack* pStack); //스택이 비어있는지 확인
+Bool Stack_IsFull(ArrStack* pStack); //스택이 가득 차 있는지 확인
+#endif
+
+//MyArrayStack.c
+#include <stdio.h>
+#include "MyArrayStack.h"
+void Stack_Init(ArrStack* pStack) //배열 스택 초기화
+{
+	pStack->top = -1;
+}
+void Stack_Push(ArrStack* pStack, Data item) //데이터 삽입
+{
+	if (Stack_IsFull(pStack) == F) {
+		pStack->top = pStack->top + 1;
+		pStack->arr[pStack->top] = item;
+	}
+	else
+		exit(1);
+}
+Data Stack_Pop(ArrStack* pStack) //데이터 인출
+{
+	if (Stack_IsEmpty(pStack) == T)
+		exit(1);
+	else
+		pStack->top = pStack->top - 1;
+		return pStack->arr[pStack->top + 1];
+}
+Data Stack_Peek(ArrStack* pStack) //최상단 데이터 확인
+{
+	if (Stack_IsEmpty(pStack) == T)
+		exit(1);
+	else
+		return pStack->arr[pStack->top];
+}
+Bool Stack_IsEmpty(ArrStack* pStack) //스택이 비어있는지 확인
+{
+	if (pStack->top == -1)
+		return T;
+	else
+		return F;
+}
+Bool Stack_IsFull(ArrStack* pStack) //스택이 가득 차 있는지 확인
+{
+	if (pStack->top == 99)
+		return T;
+	else
+		return F;
+}
+
+//main.c
 #include <stdio.h>
 #include "MyArrayStack.h"
 
