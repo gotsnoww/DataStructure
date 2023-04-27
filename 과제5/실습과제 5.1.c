d25387aa4be0e84837d4ae71095a99366374012d
gotsnoww <127213266+gotsnoww@users.noreply.github.com>
2023-04-27 15:39:30 +0900

    과제 5.1
    배열 스택 자료구조를 통해 구현된 배열 스택 프로그램이다.
    자료구조 내에는 스택을 할당할 공간(배열)과 스택의 삽입과 삭제가 이루어질 최상단 데이터인 top index가 포함되어 있다.
    헤더 파일 내에는 스택 초기화, data push, pop, 스택의 최상단 데이터 확인, 스택이 비었는지, 가득 차 있는 지를 확인하는 함수가 존재한다.
    배열 스택 내에 데이터를 push, pop 하는 동작으로 수행된다.

--- 과제5/실습과제 5.1.c
+++ 과제5/실습과제 5.1.c
@@ -28,7 +28,7 @@ void Stack_Init(ArrStack* pStack) //배열 스택 초기화
 }
 void Stack_Push(ArrStack* pStack, Data item) //데이터 삽입
 {
-	if (Stack_IsFull(pStack) == F) {
+	if (Stack_IsFull(pStack) == F) { //스택이 가득 차 있지 않을 경우에만 수행
 		pStack->top = pStack->top + 1;
 		pStack->arr[pStack->top] = item;
 	}
@@ -37,7 +37,7 @@ void Stack_Push(ArrStack* pStack, Data item) //데이터 삽입
 }
 Data Stack_Pop(ArrStack* pStack) //데이터 인출
 {
-	if (Stack_IsEmpty(pStack) == T)
+	if (Stack_IsEmpty(pStack) == T) //스택이 비었으면 종료
 		exit(1);
 	else
 		pStack->top = pStack->top - 1;
@@ -45,21 +45,21 @@ Data Stack_Pop(ArrStack* pStack) //데이터 인출
 }
 Data Stack_Peek(ArrStack* pStack) //최상단 데이터 확인
 {
-	if (Stack_IsEmpty(pStack) == T)
+	if (Stack_IsEmpty(pStack) == T) //스택이 비었으면 종료
 		exit(1);
 	else
 		return pStack->arr[pStack->top];
 }
 Bool Stack_IsEmpty(ArrStack* pStack) //스택이 비어있는지 확인
 {
-	if (pStack->top == -1)
+	if (pStack->top == -1) //top의 값이 최초 설정된 -1인지 체크
 		return T;
 	else
 		return F;
 }
 Bool Stack_IsFull(ArrStack* pStack) //스택이 가득 차 있는지 확인
 {
-	if (pStack->top == 99)
+	if (pStack->top == 99) //사용자가 지정한 배열의 크기만큼 top이 증가했는지 체크
 		return T;
 	else
 		return F;
@@ -92,3 +92,13 @@ int main(void) {
 
 	return 0;
 }
+
+
+- 과제를 대한 고찰
+스택이라는 자료 구조를 배열을 이용하여 구현하는 과제를 수행해보았다.
+임의의 크기인 배열을 미리 만들어두고, 데이터를 push pop 하는 형태의 코드이다.
+배열의 최상단인 top에서 항상 데이터의 push와 pop이 이루어지기 때문에, 수행시간은 항상 O(1)이다.
+수행공간은 배열의 크기에 따라 결정된다.
+하지만 이러한 배열공간을 사용하는 스택 자료구조는 배열이 꽉 찼을 때 새로운 데이터를 push하지 못한다.
+이러한 구조를 해결하기 위해서는 이전에 배웠던 연결리스트 자료구조를 통해 스택을 구현해야 한다.
+	
