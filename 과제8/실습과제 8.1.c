660105d8b7fca260ab8619ed27c139410458ccc4
gotsnoww <127213266+gotsnoww@users.noreply.github.com>
2023-05-16 11:31:37 +0900

    Rename 실습과제 8.1.c to 과제8/실습과제 8.1.c

--- 과제8/실습과제 8.1.c
+++ 과제8/실습과제 8.1.c
@@ -4,7 +4,7 @@
 int main(){ 
 	BT_Node* root = NULL; 
 
-	//ÀÌÁø Å½»ö Æ®¸® ±¸¼º 
+	//ì´ì§„ íƒìƒ‰ íŠ¸ë¦¬ êµ¬ì„± 
 	BST_Insert(&root, 50); 
 	BST_Insert(&root, 35); 
 	BST_Insert(&root, 70); 
@@ -15,12 +15,12 @@ int main(){
 	BST_Insert(&root, 38); 
 	BST_Insert(&root, 37); 
 
-	//±âº» ¿¬»ê ±â´É Å×½ºÆ® 
+	//ê¸°ë³¸ ì—°ì‚° ê¸°ëŠ¥ í…ŒìŠ¤íŠ¸ 
 	printf("Min value: %d \n", BST_Min(root)); 
 	printf("Max value: %d \n", BST_Max(root)); 
 	printf("Search 36 node: %d \n", BST_Search(root, 36)->data); 
-	//»èÁ¦ Å×½ºÆ® B
-	BST_Delete(&root, 35); //35, 38, 6, 50 ³ëµå »èÁ¦ Å×½ºÆ® ÁøÇà
+	//ì‚­ì œ í…ŒìŠ¤íŠ¸ B
+	BST_Delete(&root, 35); //35, 38, 6, 50 ë…¸ë“œ ì‚­ì œ í…ŒìŠ¤íŠ¸ ì§„í–‰
 	BST_Insert(&root, 39); 
 	BST_TreePrinter(root); 
 
